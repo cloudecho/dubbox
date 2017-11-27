c003e6f5418dc6ec7a8f2f87af0575c11bebdeab
cloudecho <maywide@qq.com>
2017-11-27 11:48:18 +0800

    [rpc-api] ExceptionFilter about localizedMessage

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExceptionFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ExceptionFilter.java
@@ -42,97 +42,97 @@ import com.alibaba.dubbo.rpc.service.GenericService;
  * <li>异常不在API包中，则Wrap一层RuntimeException。<br>
  * RPC对于第一层异常会直接序列化传输(Cause异常会String化)，避免异常在Client出不能反序列化问题。
  * </ol>
- * 
+ *
  * @author william.liangf
  * @author ding.lid
  */
 @Activate(group = Constants.PROVIDER)
 public class ExceptionFilter implements Filter {
 
-	private final Logger logger;
+    private final Logger logger;
 
-	public ExceptionFilter() {
-		this(LoggerFactory.getLogger(ExceptionFilter.class));
-	}
+    public ExceptionFilter() {
+        this(LoggerFactory.getLogger(ExceptionFilter.class));
+    }
 
-	public ExceptionFilter(Logger logger) {
-		this.logger = logger;
-	}
+    public ExceptionFilter(Logger logger) {
+        this.logger = logger;
+    }
 
-	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-		try {
-			Result result = invoker.invoke(invocation);
-			if (result.hasException() && GenericService.class != invoker.getInterface()) {
-				try {
-					Throwable exception = result.getException();
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        try {
+            Result result = invoker.invoke(invocation);
+            if (result.hasException() && GenericService.class != invoker.getInterface()) {
+                try {
+                    Throwable exception = result.getException();
 
-					// 如果是checked异常，直接抛出
-					if (!(exception instanceof RuntimeException) && (exception instanceof Exception)) {
-						return result;
-					}
-					// 在方法签名上有声明，直接抛出
-					try {
-						Method method = invoker.getInterface().getMethod(invocation.getMethodName(),
-								invocation.getParameterTypes());
-						Class<?>[] exceptionClassses = method.getExceptionTypes();
-						for (Class<?> exceptionClass : exceptionClassses) {
-							if (exception.getClass().equals(exceptionClass)) {
-								return result;
-							}
-						}
-					} catch (NoSuchMethodException e) {
-						return result;
-					}
+                    // 如果是checked异常，直接抛出
+                    if (!(exception instanceof RuntimeException) && (exception instanceof Exception)) {
+                        return result;
+                    }
+                    // 在方法签名上有声明，直接抛出
+                    try {
+                        Method method = invoker.getInterface().getMethod(invocation.getMethodName(),
+                                invocation.getParameterTypes());
+                        Class<?>[] exceptionClassses = method.getExceptionTypes();
+                        for (Class<?> exceptionClass : exceptionClassses) {
+                            if (exception.getClass().equals(exceptionClass)) {
+                                return result;
+                            }
+                        }
+                    } catch (NoSuchMethodException e) {
+                        return result;
+                    }
 
-					// #CTOWER
-					String className = exception.getClass().getName();
-					if (className.startsWith("ec.ctower.")) {
-						return new RpcResult(new RuntimeException(
-								"@CTOWER#" + exception.getClass().getName() + "#" + exception.getLocalizedMessage()));
-					}
+                    // Localized
+                    final String localizedMessage = exception.getLocalizedMessage();
+                    if (localizedMessage != null && localizedMessage.startsWith("localized://")) {
+                        return new RpcResult(new RuntimeException(localizedMessage));
+                    }
 
-					// 未在方法签名上定义的异常，在服务器端打印ERROR日志
-					logger.error(
-							"Got unchecked and undeclared exception which called by "
-									+ RpcContext.getContext().getRemoteHost() + ". service: "
-									+ invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
-									+ ", exception: " + exception.getClass().getName() + ": " + exception.getMessage(),
-							exception);
+                    // 未在方法签名上定义的异常，在服务器端打印ERROR日志
+                    logger.error(
+                            "Got unchecked and undeclared exception which called by "
+                                    + RpcContext.getContext().getRemoteHost() + ". service: "
+                                    + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
+                                    + ", exception: " + exception.getClass().getName() + ": " + exception.getMessage(),
+                            exception);
 
-					// 异常类和接口类在同一jar包里，直接抛出
-					String serviceFile = ReflectUtils.getCodeBase(invoker.getInterface());
-					String exceptionFile = ReflectUtils.getCodeBase(exception.getClass());
-					if (serviceFile == null || exceptionFile == null || serviceFile.equals(exceptionFile)) {
-						return result;
-					}
-					// 是JDK自带的异常，直接抛出
-					if (className.startsWith("java.") || className.startsWith("javax.")) {
-						return result;
-					}
+                    // 异常类和接口类在同一jar包里，直接抛出
+                    String serviceFile = ReflectUtils.getCodeBase(invoker.getInterface());
+                    String exceptionFile = ReflectUtils.getCodeBase(exception.getClass());
+                    if (serviceFile == null || exceptionFile == null || serviceFile.equals(exceptionFile)) {
+                        return result;
+                    }
+                    // 是JDK自带的异常，直接抛出
+                    String className = exception.getClass().getName();
+                    if (className.startsWith("java.") || className.startsWith("javax.")) {
+                        return result;
+                    }
 
-					// 是Dubbo本身的异常，直接抛出
-					if (exception instanceof RpcException) {
-						return result;
-					}
+                    // 是Dubbo本身的异常，直接抛出
+                    if (exception instanceof RpcException) {
+                        return result;
+                    }
 
-					// 否则，包装成RuntimeException抛给客户端
-					return new RpcResult(new RuntimeException(StringUtils.toString(exception)));
-				} catch (Throwable e) {
-					logger.warn("Fail to ExceptionFilter when called by " + RpcContext.getContext().getRemoteHost()
-							+ ". service: " + invoker.getInterface().getName() + ", method: "
-							+ invocation.getMethodName() + ", exception: " + e.getClass().getName() + ": "
-							+ e.getMessage(), e);
-					return result;
-				}
-			}
-			return result;
-		} catch (RuntimeException e) {
-			logger.error("Got unchecked and undeclared exception which called by "
-					+ RpcContext.getContext().getRemoteHost() + ". service: " + invoker.getInterface().getName()
-					+ ", method: " + invocation.getMethodName() + ", exception: " + e.getClass().getName() + ": "
-					+ e.getMessage(), e);
-			throw e;
-		}
-	}
+                    // 否则，包装成RuntimeException抛给客户端
+                    return new RpcResult(new RuntimeException(StringUtils.toString(exception)));
+                } catch (Throwable e) {
+                    logger.warn("Fail to ExceptionFilter when called by " + RpcContext.getContext().getRemoteHost()
+                            + ". service: " + invoker.getInterface().getName() + ", method: "
+                            + invocation.getMethodName() + ", exception: " + e.getClass().getName() + ": "
+                            + e.getMessage(), e);
+                    return result;
+                }
+            }
+            return result;
+        } catch (RuntimeException e) {
+            logger.error("Got unchecked and undeclared exception which called by "
+                    + RpcContext.getContext().getRemoteHost() + ". service: " + invoker.getInterface().getName()
+                    + ", method: " + invocation.getMethodName() + ", exception: " + e.getClass().getName() + ": "
+                    + e.getMessage(), e);
+            throw e;
+        }
+    }
 
 }
